042f359a69f7d38977035fbae1e63cc8adcb4a81
kosta891 <kosta891@gmail.com>
2021-10-29 23:13:00 +0200

    setting react router

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage';
+import Product from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import Private from './PrivateRoute';
+
+export { Home, About, Product, SingleProduct, Error, Private, Cart, Checkout };
