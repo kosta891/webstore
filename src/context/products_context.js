40184fae45e1300eb099a716fbd94313d268ea97
kosta891 <kosta891@gmail.com>
2021-11-06 23:20:24 +0100

    single product fetch

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: [],
 };
 
 const ProductsContext = React.createContext();
@@ -43,13 +46,27 @@ export const ProductsProvider = ({ children }) => {
       dispatch({ type: GET_PRODUCTS_ERROR });
     }
   };
-  console.log(state.products, state.featured_products);
+
+  const fetchSingleProduct = async (url) => {
+    try {
+      dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+      const response = await axios(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
+
+  console.log(state.single_product);
   useEffect(() => {
     fetchProducts(url);
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,8 @@
 export const formatPrice = (number) => {
-  const newNubmer = Intl.NumberFormat('en-US', {
+  return Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   }).format(number / 100);
-
-  return newNubmer;
 };
 
 export const getUniqueValues = () => {};
