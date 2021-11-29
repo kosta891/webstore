3dbe2b68765197898884d4c28bc78553901afd9b
kosta891 <kosta891@gmail.com>
2021-11-29 21:05:29 +0100

    stripe

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-yn693p1d.us.auth0.com'
-    clientId='thdAMapxJE2UL5CKGLW4UcYpzKfiP0ho'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
